ba382b7c13cde16b3706806cc76367ded42df286
hitoshi <hitoshiaragashi@gmail.com>
2026-09-11 00:11:05 +0700

    Create math.cpp

---
+++ math.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+
+int main() {
+  int a, b; 
+  a = 90;
+  b = 75;
+
+  if(a > b) {
+    std::cout<<"You Got A Plus!!"<<std::endl;
+  } else {
+    std::cout<<"Study More!"<<std::endl;
+  }
+return 0;
+}
